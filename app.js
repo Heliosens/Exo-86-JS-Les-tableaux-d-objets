26828d203807b1b5970e3eeabce5c32377edb70e
Sylvie-B <unsolo@hotmail.fr>
2021-11-30 13:00:06 +0100

    creation de span pour chaque couleur

--- app.js
+++ app.js
@@ -31,10 +31,18 @@ let voitures = {
     garage: 'concession',
     lieu: 'Fourmies',
     // Dans un tableau, ajouter les différentes voitures que vous possédez.
-    collection: [], // Vos voitures dans cette propriété !
+    collection: [citroen, peugeot, ford], // Vos voitures dans cette propriété !
 }
 
 // FIXME Parcourrez le tableau dans lequel vous avez placé vos objets.
 // TODO ==> invoquez la méthode ouvrirPorte pour chaque voiture de la collection.
 
+let p = document.querySelector('p');
+
+for( let item of voitures.collection){
+    console.log(item.ouvrirPorte());
+    let newSpan = document.createElement('span');
+    newSpan.innerHTML = item.color + " ";
+    p.appendChild(newSpan);
+}
 
